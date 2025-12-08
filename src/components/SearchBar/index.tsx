e53d822fac3f6da17ccb2eb221b7f67e788a52ec
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-07 22:56:58 -0300

    Catalog page and its components

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function SearchBar() {
+  return (
+    <form className="dsc-search-bar">
+      <button type="submit">🔎︎</button>
+      <input type="text" placeholder="Nome do produto" />
+      <button type="reset">❌</button>
+    </form>
+  );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,33 @@
+import "./style.css";
+import HeaderClient from "../../components/HeaderClient";
+import SearchBar from "../../components/SearchBar";
+import CatalogCard from "../../components/CatalogCard";
+import ButtonNextPage from "../../components/ButtonNextPage";
+
+export default function Catalog() {
+  return (
+    <>
+      <main>
+        <HeaderClient />
+        <section id="catalog-section" className="dsc-container">
+          <SearchBar />
+          <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+            <CatalogCard />
+          </div>
+
+          <ButtonNextPage />
+        </section>
+      </main>
+    </>
+  );
+}
