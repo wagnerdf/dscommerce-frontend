f656c30281716270fa2709837b46a31b1774209f
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-12 13:00:51 -0300

    Catalog card - Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import ProductDetails from "./routes/ProductDetails";
+import Catalog from "./routes/Catalog";
 
 export default function App() {
-  return <ProductDetails />;
+  return <Catalog />;
 }
--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,15 +1,19 @@
 import "./styles.css";
-import computerImg from '../../assets/computer.png';
+import type { ProductDTO } from "../../models/product";
 
-export default function CatalogCard() {
+type Props = {
+  product: ProductDTO;
+};
+
+export default function CatalogCard({ product }: Props) {
   return (
     <div className="dsc-card">
       <div className="dsc-catalog-carg-top dsc-line-bottom">
-        <img src={computerImg} alt="Computer" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="dsc-catalog-carg-bottom">
-        <h3>R$ 5000,00</h3>
-        <h4>Computador Gamer XT</h4>
+        <h3>R$ {product.price.toFixed(2)}</h3>
+        <h4>{product.name}</h4>
       </div>
     </div>
   );
