87efbf5ddb30250cc265c735bfd071943f6225a1
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-12 12:17:12 -0300

    Props

---
+++ src/models/product.ts
@@ -0,0 +1,10 @@
+import type { CategoryDTO } from "./category";
+
+export type ProductDTO = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgUrl: string;
+    categories: CategoryDTO[];
+}
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,29 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import type { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Tv com recursos da nova tecnologia  TV 3.0, ou DTV+, que integra a TV aberta tradicional (antena) com a internet para oferecer imagem 4K, som de cinema, e recursos interativos como votações em tempo real",
+  imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos"
+    },
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+}
 
 export default function ProductDetails() {
   return (
@@ -10,7 +33,7 @@ export default function ProductDetails() {
       <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
+          <ProductDetailsCard product={product}/>
           <div className="dsc-btn-page-container">
             <ButtonPrimary />
             <ButtonInverse />
