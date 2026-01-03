ab437f6a5587bb79dd6231056765ab5c50f6b902
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-02 22:45:00 -0300

    Requests for the catalog and by ID

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,14 +3,25 @@ import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from "../../../services/product-service";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 export default function Catalog() {
+  const [products, setProucts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?size=12").then((response) => {
+      setProucts(response.data.content);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          {productService.findAll().map((product) => (
+          {products.map(product => (
             <CatalogCard key={product.id} product={product} />
           ))}
         </div>
