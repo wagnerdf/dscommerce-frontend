e01ae87cec6a395788cea998d4c580de9005666e
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-04 13:06:49 -0300

    Catch request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,12 +3,14 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
+
+  const navigate = useNavigate();
 
   const [product, setProduct] = useState<ProductDTO>();
 
@@ -17,13 +19,18 @@ export default function ProductDetails() {
       .then(response => {
         setProduct(response.data);
       })
+      .catch(() => {
+        navigate("/");
+      });
   }, []);
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {product && <ProductDetailsCard product={product} />}
-
+        {
+          product &&
+          <ProductDetailsCard product={product} />
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <Link to="/">
