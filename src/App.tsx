71dac6875d4770ba6236bc0ca9d3f4839775ec16
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-07 13:47:20 -0300

    Shopping cart screen layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Catalog from "./routes/ClientHome/Catalog";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
           <Route path="product-details/:productId" element={<ProductDetails />}></Route>
+          <Route path="cart" element={<Cart />} />
         </Route>
         <Route path="*" element={<Navigate to="/"/ >} />
       </Routes>
