955c3aa69f58e7c6d0f746512ff7d0ef73b636bb
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-08 14:02:11 -0300

    Increase the quantity of items in the cart.

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -12,6 +12,11 @@ export default function Cart() {
     setCart(cartService.getCart());
   }
 
+  function handleIncreaseItem(productId: number){
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -33,7 +38,7 @@ export default function Cart() {
                     <div className="dsc-cart-item-quantity-container">
                       <div className="dsc-cart-item-quantity-btn">-</div>
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div onClick={() => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
                     </div>
                   </div>
                 </div>
