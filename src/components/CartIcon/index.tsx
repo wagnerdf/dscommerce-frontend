99256f7c0df1ca856e02bd9a902064404359b7ba
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-10 11:39:48 -0300

    Retrieving data from localStorage

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,11 +1,16 @@
 import cartIcon from "../../assets/cart.svg";
 import "./styles.css";
+import * as cartService from '../../services/cart-service';
+import { useState } from "react";
 
 export default function CartIcon() {
+
+    const [cart, setCart] = useState(cartService.getCart());
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">22</div>
+      <div className="dsc-cart-count">{cart.items.length}</div>
     </>
   );
 }
