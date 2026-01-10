f3bec1ee073591f8d0c7c35ad629da57ccecf5f1
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-10 12:45:02 -0300

    Creating and providing global state with the context API

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import ClientHome from "./routes/ClientHome";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Catalog from "./routes/ClientHome/Catalog";
 import Cart from "./routes/ClientHome/Cart";
+import { useState } from "react";
+import { ContextCartCount } from "./utils/context-cart";
 
 export default function App() {
+
+  const [contextCartCount, setContextCartCount] = useState<number>(0);
+
   return (
+    <ContextCartCount.Provider value={{contextCartCount, setContextCartCount}}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClientHome />}>
@@ -17,5 +23,6 @@ export default function App() {
         <Route path="*" element={<Navigate to="/"/ >} />
       </Routes>
     </BrowserRouter>
+    </ContextCartCount.Provider>
   );
 }
