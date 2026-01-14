f96fb86df027b41a0f5204f4deaa8fdda92ab3d8
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-14 11:28:15 -0300

    Axios Interceptors

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,17 +1,44 @@
 import type { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
 import axios from "axios";
-import * as authService from "../services/auth-service"
+import * as authService from "../services/auth-service";
 
 export function requestBackend(config: AxiosRequestConfig) {
-
-    const headers = config.withCredentials
+  const headers = config.withCredentials
     ? {
         ...config.headers,
         Authorization: "Bearer " + authService.getAccessToken(),
-    }
-    : config.headers
-
+      }
+    : config.headers;
 
   return axios({ ...config, baseURL: BASE_URL, headers });
 }
+
+// REQUEST INTERCEPTOR
+axios.interceptors.request.use(
+  function (config) {
+    // DO SOMETHING BEFORE REQUEST IS SENT
+    return config;
+  },
+  function (error) {
+    // DO SOMETHING WITH REQUEST ERROR
+    return Promise.reject(error);
+  }
+);
+
+// RESPONSE INTERCEPTOR
+axios.interceptors.response.use(
+  function (response) {
+    // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      console.log("DEU 401!!!");
+    }
+    if (error.response.status === 403) {
+      console.log("DEU 403!!!");
+    }
+    return Promise.reject(error);
+  }
+);
