d9d27e34dce3dbcec9f79721649037c991e0c264
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-20 13:34:02 -0300

    isAuthenticated

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,6 +5,7 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
+import { isAuthenticated } from "../../../services/auth-service";
 
 type QueryParams = {
   page: number;
@@ -23,6 +24,7 @@ export default function Catalog() {
   });
 
   useEffect(() => {
+    console.log("AUTENTICADO", isAuthenticated());
     productService.findPageRequest(queryParams.page, queryParams.name).then((response) => {
       const nextPage = response.data.content; 
       setProducts(products.concat(nextPage));
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -49,3 +49,11 @@ export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
     return undefined;
   }
 }
+
+export function isAuthenticated(): boolean {
+  let tokenPayload = getAccessTokenPayload();
+  if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
+    return true;
+  }
+  return false;
+}
