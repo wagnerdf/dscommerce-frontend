b3d3c73d2ff3e12d3ba9194303c7e34de649a3cf
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-21 10:52:30 -0300

    Context token payload

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,10 +1,13 @@
 import "./styles.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import type { CredentialsDTO } from "../../../models/auth";
 import * as authService from "../../../services/auth-service"
 import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
 
 export default function Login() {
+
+  const { setContextTokenPayload } = useContext(ContextToken);
 
   const navigate = useNavigate();
 
@@ -18,6 +21,7 @@ export default function Login() {
     authService.loginRequest(formData)
         .then(response => {
             authService.saveAccessToken(response.data.access_token);
+            setContextTokenPayload(authService.getAccessTokenPayload());
             navigate("/cart");
         })
         .catch(error => {
