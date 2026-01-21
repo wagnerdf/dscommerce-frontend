7d56865f517a8a6f6820c0963e5e826ce9e82e30
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-21 13:30:39 -0300

    Place order

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,11 +1,15 @@
 import { useContext, useState } from "react";
 import "./styles.css";
 import * as cartService from "../../../services/cart-service";
+import * as orderService from "../../../services/order-service";
 import type { OrderDTO } from "../../../models/order";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ContextCartCount } from "../../../utils/context-cart";
 
 export default function Cart() {
+
+  const navigate = useNavigate();
+
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
   const {setContextCartCount} = useContext(ContextCartCount);
@@ -30,6 +34,15 @@ export default function Cart() {
         const newCart = cartService.getCart();
     setCart(newCart);
     setContextCartCount(newCart.items.length);
+  }
+
+  function handlePlaceOrderClick(){
+    orderService.placeOrderRequest(cart)
+      .then(response => {
+        cartService.clearCart();
+        setContextCartCount(0);
+        navigate(`/confirmation/${response.data.id}`)
+      });
   }
 
   return (
@@ -69,7 +82,7 @@ export default function Cart() {
           </div>
         )}
         <div className="dsc-btn-page-container">
-          <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
+          <div onClick={handlePlaceOrderClick} className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
           <Link to="/catalog">
             <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
           </Link>
--- src/services/order-service.ts
+++ src/services/order-service.ts
@@ -1,10 +1,22 @@
 import type { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import type { OrderDTO } from "../models/order";
 
 export function findByIdRequest(id: number) {
   const config: AxiosRequestConfig = {
     url: `/orders/${id}`,
     withCredentials: true,
+  };
+
+  return requestBackend(config);
+}
+
+export function placeOrderRequest(cart: OrderDTO) {
+  const config: AxiosRequestConfig = {
+    url: "/orders",
+    method: "POST",
+    withCredentials: true,
+    data: cart,
   };
 
   return requestBackend(config);
