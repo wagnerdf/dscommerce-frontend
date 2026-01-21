afad38e87bcd2b5fcaa9718e5928a78c5c275dfd
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-21 12:42:46 -0300

    Confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import type { AccessTokenPayloadDTO } from "./models/auth";
 import { ContextToken } from "./utils/context-token";
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
+import Confirmation from "./routes/ClientHome/Confirmation";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -43,12 +44,10 @@ export default function App() {
             <Route path="/" element={<ClientHome />}>
               <Route index element={<Catalog />} />
               <Route path="catalog" element={<Catalog />} />
-              <Route
-                path="product-details/:productId"
-                element={<ProductDetails />}
-              ></Route>
+              <Route path="product-details/:productId" element={<ProductDetails />} />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
+              <Route path="confirmation/:orderId" element={<Confirmation />} />
             </Route>
             <Route
               path="/admin/"
