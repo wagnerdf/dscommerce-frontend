c2195a61e5e42d1906db02898eb681944e74989e
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-22 14:29:49 -0300

    Dialog modal info

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -14,6 +14,12 @@ type QueryParams = {
 };
 
 export default function ProductListing() {
+
+  const [dialogInfoData, setDialogInfoData] = useState({
+    visible: false,
+    message: "Operação com sucesso"
+  })
+
   const [isLastPage, setIsLastPage] = useState(false);
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
@@ -40,6 +46,14 @@ export default function ProductListing() {
 
   function handleNextPageClick(){
     setQueryParams({ ...queryParams, page: queryParams.page + 1})
+  }
+
+  function handleDialogInfoClose(){
+    setDialogInfoData({...dialogInfoData, visible: false})
+  }
+
+  function handleDeleteClick(){
+    setDialogInfoData({...dialogInfoData, visible: true})
   }
 
   return (
@@ -72,7 +86,7 @@ export default function ProductListing() {
                 <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
                 <td className="dsc-txt-left">{product.name}</td>
                 <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+                <td><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
               </tr>
             ))}
           </tbody>
@@ -82,9 +96,16 @@ export default function ProductListing() {
           <div onClick={handleNextPageClick}>
             <ButtonNextPage />
           </div>
-          }
+        }
       </section>
-      <DialogInfo />
+
+      {
+        dialogInfoData.visible &&
+        <DialogInfo 
+          message={dialogInfoData.message}
+          onDialogClose={handleDialogInfoClose}
+        />
+      }
     </main>
   );
 }
