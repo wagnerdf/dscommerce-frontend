fb592103a278e29d6b25a9dfce2e5d60a38a34a8
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-22 12:42:58 -0300

    Dynamic product listing

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -1,11 +1,37 @@
+import "./styles.css";
+import * as productService from "../../../services/product-service";
 import editIcon from "../../../assets/edit.svg";
 import deleteIcon from "../../../assets/delete.svg";
-import product from "../../../assets/computer.png";
-import "./styles.css";
+import { useEffect, useState } from "react";
+import type { ProductDTO } from "../../../models/product";
 
-export default function ProductListing(){
-    return (
-         <main>
+type QueryParams = {
+  page: number;
+  name: string;
+};
+
+export default function ProductListing() {
+  const [isLastPage, setIsLastPage] = useState(false);
+
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: "",
+  });
+
+  useEffect(() => {
+    productService
+      .findPageRequest(queryParams.page, queryParams.name)
+      .then((response) => {
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
+      });
+  }, [queryParams]);
+
+  return (
+    <main>
       <section id="product-listing-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
@@ -27,39 +53,25 @@ export default function ProductListing(){
               <th className="dsc-tb768">Preço</th>
               <th className="dsc-txt-left">Nome</th>
               <th></th>
-              <th></th>  
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="dsc-tb576">341</td>
-              <td><img className="dsc-product-listing-image" src={product} alt="Computer" /></td>
-              <td className="dsc-tb768">R$ 5000,00</td>
-              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-              <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-              <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
-            </tr>
-            <tr>
-              <td className="dsc-tb576">341</td>
-              <td><img className="dsc-product-listing-image" src={product} alt="Computer" /></td>
-              <td className="dsc-tb768">R$ 5000,00</td>
-              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-              <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-              <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
-            </tr>
-            <tr>
-              <td className="dsc-tb576">341</td>
-              <td><img className="dsc-product-listing-image" src={product} alt="Computer" /></td>
-              <td className="dsc-tb768">R$ 5000,00</td>
-              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-              <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-              <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
-            </tr>
+            {products.map((product) => (
+              <tr>
+                <td className="dsc-tb576">{product.id}</td>
+                <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name} /></td>
+                <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
+                <td className="dsc-txt-left">{product.name}</td>
+                <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
+                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+              </tr>
+            ))}
           </tbody>
         </table>
 
         <div className="dsc-btn-next-page">Carregar mais</div>
       </section>
     </main>
-    );
+  );
 }
