f3e98ef162ee105d7a4f374bb6e9a72266d8347f
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-22 11:08:57 -0300

    Preparing the routes for the administrative area

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { ContextToken } from "./utils/context-token";
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
 import Confirmation from "./routes/ClientHome/Confirmation";
+import ProductListing from "./routes/Admin/ProductListing";
+import ProductForm from "./routes/Admin/ProductForm";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -49,15 +51,11 @@ export default function App() {
               <Route path="login" element={<Login />} />
               <Route path="confirmation/:orderId" element={<PrivateRoute><Confirmation /></PrivateRoute>} />
             </Route>
-            <Route
-              path="/admin/"
-              element={
-                <PrivateRoute roles={["ROLE_ADMIN"]}>
-                  <Admin />
-                </PrivateRoute>
-              }
-            >
-              <Route index element={<AdminHome />} />
+            <Route path="/admin/" element={<PrivateRoute roles={["ROLE_ADMIN"]}><Admin /></PrivateRoute>}>
+              <Route index element={<Navigate to="/admin/home" />} />
+              <Route path="home" element={<AdminHome />} />
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
