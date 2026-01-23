01e56e1c95d6ca3a539a936a510d7afd23c8290e
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-23 12:56:38 -0300

    forms updateAll

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import type { ProductDTO } from "../../../models/product";
 import SearchBar from "../../../components/SearchBar";
 import DialogInfo from "../../../components/DialogInfo";
 import DialogConfirmation from "../../../components/DialogConfirmation";
+import ButtonInverse from "../../../components/ButtonInverse";
+import { useNavigate } from "react-router-dom";
 
 type QueryParams = {
   page: number;
@@ -16,13 +18,15 @@ type QueryParams = {
 
 export default function ProductListing() {
 
+  const navigate = useNavigate();
+
   const [dialogConfirmationData, setDialogConfirmationData] = useState({
     visible: false,
     id: 0,
     message: "Tem certeza?"
   })
 
-    const [dialogInfoData, setDialogInfoData] = useState({
+  const [dialogInfoData, setDialogInfoData] = useState({
     visible: false,
     message: "Operação com sucesso"
   })
@@ -46,31 +50,35 @@ export default function ProductListing() {
       });
   }, [queryParams]);
 
-  function handleSearch(searchText: string){
+  function handleNewProductClick(){
+    navigate("/admin/products/create");
+  }
+
+  function handleSearch(searchText: string) {
     setProducts([]);
-    setQueryParams({ ...queryParams, page: 0, name: searchText});
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
   }
 
-  function handleNextPageClick(){
-    setQueryParams({ ...queryParams, page: queryParams.page + 1})
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 })
   }
 
-  function handleDialogInfoClose(){
-    setDialogInfoData({...dialogInfoData, visible: false})
+  function handleDialogInfoClose() {
+    setDialogInfoData({ ...dialogInfoData, visible: false })
   }
 
-  function handleDeleteClick(productId: number){
-    setDialogConfirmationData({...dialogConfirmationData, id: productId, visible: true})
+  function handleDeleteClick(productId: number) {
+    setDialogConfirmationData({ ...dialogConfirmationData, id: productId, visible: true })
   }
 
-  function handleDialogConfirmationAnswer(answer: boolean, productId: number){
-    if(answer){
+  function handleDialogConfirmationAnswer(answer: boolean, productId: number) {
+    if (answer) {
       productService.deleteById(productId)
         .then(() => {
           setProducts([]);
-          setQueryParams({ ...queryParams, page: 0});
+          setQueryParams({ ...queryParams, page: 0 });
         })
-        .catch(error =>{
+        .catch(error => {
           setDialogInfoData({
             visible: true,
             message: error.response.data.error
@@ -78,7 +86,7 @@ export default function ProductListing() {
         });
     }
     console.log("Resposta", answer);
-    setDialogConfirmationData({...dialogConfirmationData, visible: false})
+    setDialogConfirmationData({ ...dialogConfirmationData, visible: false })
   }
 
   return (
@@ -87,7 +95,9 @@ export default function ProductListing() {
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
         <div className="dsc-btn-page-container dsc-mb20">
-          <div className="dsc-btn dsc-btn-white">Novo</div>
+          <div onClick={handleNewProductClick}>
+            <ButtonInverse text="Novo" />
+          </div>
         </div>
 
         <SearchBar onSearch={handleSearch} />
@@ -126,7 +136,7 @@ export default function ProductListing() {
 
       {
         dialogInfoData.visible &&
-        <DialogInfo 
+        <DialogInfo
           message={dialogInfoData.message}
           onDialogClose={handleDialogInfoClose}
         />
@@ -135,7 +145,7 @@ export default function ProductListing() {
       {
         dialogConfirmationData.visible &&
         <DialogConfirmation
-          id={dialogConfirmationData.id} 
+          id={dialogConfirmationData.id}
           message={dialogConfirmationData.message}
           onDialogAnswer={handleDialogConfirmationAnswer}
         />
