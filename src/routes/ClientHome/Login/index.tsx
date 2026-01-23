9b75c5c45233f279d22eb77a759396b0183680b2
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-23 11:26:15 -0300

    forms update

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { useContext, useState } from "react";
 import * as authService from "../../../services/auth-service";
+import * as forms from "../../../utils/forms";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
 import FormInput from "../../../components/FormInput";
@@ -51,9 +52,7 @@ export default function Login() {
   }
 
   function handleInputChange(event: any) {
-    const value = event.target.value;
-    const name = event.target.name;
-    setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+    setFormData(forms.update(formData, event.target.name, event.target.value));
   }
 
   return (
