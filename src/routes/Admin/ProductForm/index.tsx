f4e1b157903c15bea850f8b7a99b7213969859a9
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-24 14:56:00 -0300

    FormTextArea

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import * as forms from "../../../utils/forms";
 import FormInput from "../../../components/FormInput";
 import * as productService from "../../../services/product-service";
+import FormTextArea from "../../../components/FormTextArea";
 
 export default function ProductForm() {
 
@@ -18,7 +19,7 @@ export default function ProductForm() {
       name: "name",
       type: "text",
       placeholder: "Nome",
-      validation: function (value: string){
+      validation: function (value: string) {
         return /^.{3,80}$/.test(value);
       },
       message: "Favor informar um nome de 3 a 80 caracteres"
@@ -41,6 +42,17 @@ export default function ProductForm() {
       type: "text",
       placeholder: "Imagem",
     },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function (value: string) {
+        return /^.{10,}$/.test(value);
+      },
+      message: "A descrição deve ter pelo menos 10 caracteres"
+    }
   });
 
   useEffect(() => {
@@ -95,8 +107,16 @@ export default function ProductForm() {
                   onChange={handleInputChange}
                 />
               </div>
+              <div>
+                <FormTextArea
+                  {...formData.description}
+                  className="dsc-form-control dsc-textarea"
+                  onTurnDirty={handleTurnDirty}
+                  onChange={handleInputChange}
+                />
+                <div className="dsc-form-error">{formData.description.message}</div>
+              </div>
             </div>
-
             <div className="dsc-product-form-buttons">
               <Link to="/admin/products">
                 <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
