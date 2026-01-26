a92b19f941cd773cb00dcda5904eef892b8c6e25
Wagner Andrade <wagnerandrade_df@hotmail.com>
2026-01-26 12:23:12 -0300

    Backend errors

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -121,7 +121,12 @@ export default function ProductForm() {
     request
       .then(() => {
         navigate("/admin/products");
-      });
+      })
+      .catch(error => {
+        const newInputs = forms.setBackendErrors(formData, error.response.data.errors);
+        setFormData(newInputs);
+        //console.log(error.response.data.errors);
+      })
 
     console.log(requestBoody);
   }
